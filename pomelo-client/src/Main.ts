376dae7e85e0403ef06aa86b430cd0b68ae885c2
Bob Jiang <dashaomai@gmail.com>
2015-04-24 16:30:52 +0800

    同时展示了侦听服务器摄像头的消息及主动发出 request 的功能

--- pomelo-client/src/Main.ts
+++ pomelo-client/src/Main.ts
@@ -175,7 +175,7 @@ class Main extends egret.DisplayObjectContainer {
                     }
                 );
             }
-        )
+        );
     }
     /**
      * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
